2987092d0bc5862212de42124d75f281e02a3099
Riley Griffis <ragriffis@gmail.com>
2022-07-12 12:56:36 -0400

    Update various content/social icons

--- Components/footerCities.js
+++ Components/footerCities.js
@@ -26,178 +26,171 @@
 <div class="fl-row fl-row-full-width fl-row-bg-color fl-node-59078ee665034" data-node="59078ee665034">
 <div class="fl-row-content-wrap">
 <div class="uabb-row-separator uabb-top-row-separator"></div>
+
+
+
+
 <div class="fl-row-content fl-row-fixed-width fl-node-content">
-<div class="fl-col-group fl-node-59078ee664db2"data-node="59078ee664db2">
-  <div class="fl-col fl-node-59078ee664dfe fl-col-has-cols" data-node="59078ee664dfe">
-    <div class="fl-col-content fl-node-content">
+                  <div class="fl-col-group fl-node-59078ee664db2" data-node="59078ee664db2">
+                    <div class="fl-col fl-node-59078ee664dfe fl-col-has-cols" data-node="59078ee664dfe">
+                      <div class="fl-col-content fl-node-content">
+            
+            
+                        <!-----------Start(X X X)Footer----------------->
+                        <div class="fl-col-group fl-node-59078ee664e7e fl-col-group-nested" data-node="59078ee664e7e">
+            
+            
+                          <!--Footer (0 0 X)-->
+                          <div class="fl-col fl-node-59078ee664ef4 fl-col-small" data-node="59078ee664ef4">
+                            <div class="fl-col-content fl-node-content">
+                              <!--Reach Out Contact Info-->
+                              <div class="fl-module fl-module-rich-text fl-node-59079189674fc" data-node="59079189674fc">
+                                <div class="fl-module-content fl-node-content">
+                                  <div class="fl-rich-text">
+    
+                                    <h4>Reach Out</h4>
+
+                                    <div class="fl-rich-text">
+                                    <p>
+                                      Snap Booth Photos<br />
+                                      <a href="tel:">(734) 707-7794</a> <br/>
+
+                                    <!--FaceBook Icon/Link-->
+                                    <span class="pp-social-icon"itemscope itemtype="http://schema.org/Organization">
+                                      <link itemprop="url" href=""/>
+                                      <a itemprop="sameAs" href="https://www.facebook.com/snapboothphotos" target="_blank" title="Facebook"
+                                      aria-label="Facebook" role="button" rel="noopener" style="padding-right: 5px;">
+                                      <i class="fa fa-facebook"></i>
+                                      </a>
+                                    </span>
 
 
-      <!-----------Start(X X X)Footer----------------->
-      <div class="fl-col-group fl-node-59078ee664e7e fl-col-group-nested" data-node="59078ee664e7e">
-        <!--Footer (X 0 0)-->
-        <div class="fl-col fl-node-59078ee664ebb fl-col-small" data-node="59078ee664ebb">
-          <div class="fl-col-content fl-node-content">
-            <div class="fl-module fl-module-rich-text fl-node-59079271d91c2" data-node="59079271d91c2">
-              <div class="fl-module-content fl-node-content">
-                <div class="fl-rich-text">
 
-                  <!--Event Type Site Links-->
-                  <h4>Event Types</h4>
-                  <ul>
-                    <li><a href="Corporate-Rentals.html">Corporate Events</a></li>
-                    <li><a href="Wedding-Rentals.html">Weddings</a></li>
-                    <li><a href="Party-Rentalss.html">Parties</a></li>
-                    <li><a href="Graduation-Rentals.html">Graduation Parties</a></li>
-                    <li>Charity Functions</li>
-                    <li>Bar &amp; Bat Mitzvahs</li>
-                    <li>School Events &amp; Prom</li> 
-                  </ul>
+                                    <!--Twitter Icon/Link-->
+                                    <span class="pp-social-icon"itemscope itemtype="http://schema.org/Organization">
+                                      <link itemprop="url" href=""/>
+                                      <a itemprop="sameAs" href="https://www.instagram.com/snapboothphotos/" target="_blank" title="Instagram"
+                                      aria-label="Instagram" role="button" rel="noopener" style="padding-right: 5px;">
+                                      <i class="fa fa-instagram"></i>
+                                      </a>
+                                    </span>
 
+                                    <!--Youtube Icon/Link
+                                    <span class="pp-social-icon"itemscope itemtype="http://schema.org/Organization">
+                                      <link itemprop="url" href=""/>
+                                      <a class = "pp-social-icon" itemprop="sameAs" href="https://www.youtube.com/" target="_blank" title="YouTube"
+                                      aria-label="YouTube" role="button" rel="noopener">
+                                      <i class="fa fa-youtube"></i>
+                                      </a>
+                                    </span>
+                                    -->
+
+
+
+
+                                    </p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+            
+            
+            
+            
+            
+                          <!--Footer (X 0 0)-->
+                          <div class="fl-col fl-node-59078ee664ebb fl-col-small" data-node="59078ee664ebb">
+                            <div class="fl-col-content fl-node-content">
+                              <div class="fl-module fl-module-rich-text fl-node-59079271d91c2" data-node="59079271d91c2">
+                                <div class="fl-module-content fl-node-content">
+                                  <div class="fl-rich-text">
+            
+                                    <!--Event Type Site Links-->
+                                    <h4>Event Types</h4>
+                                    <ul>
+                                      <li><a href="Corporate-Rentals.html">Corporate Events</a></li>
+                                      <li><a href="Wedding-Rentals.html">Weddings</a></li>
+                                      <li><a href="Party-Rentalss.html">Parties</a></li>
+                                      <li><a href="Graduation-Rentals.html">Graduation Parties</a></li>
+                                      <li>Charity Functions</li>
+                                      <li>Bar &amp; Bat Mitzvahs</li>
+                                      <li>School Events &amp; Prom</li>
+                                    </ul>
+            
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+            
+                          <!--Footer (0 X 0)-->
+                          <div class="fl-col fl-node-59078ee664f2e fl-col-small" data-node="59078ee664f2e">
+                            <div class="fl-col-content fl-node-content">
+                              <div class="fl-module fl-module-rich-text fl-node-59078ee664ffa" data-node="59078ee664ffa">
+                                <div class="fl-module-content fl-node-content">
+                                  <div class="fl-rich-text">
+                                    <!--Site Links-->
+                                    <h4>Links</h4>
+                                    <ul>
+                                      <li><a href="/RatesAvailability.html">Contact</a></li>
+                                      <li> <a href="/FAQs.html">FAQ</a> </li>
+                                      <li> <a href="/cities.html">Cities We Serve</a> </li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+            
+            
+                        </div>
+                        <!-------------End(X X X)Footer----------------->
+            
+            
+            
+                        <div class="fl-module fl-module-rich-text fl-node-59078ee664e3f" data-node="59078ee664e3f">
+                          <div class="fl-module fl-module-rich-text fl-node-59078ee664e3f" data-node="59078ee664e3f">
+                            <div class="fl-module-content fl-node-content">
+                              <div class="fl-rich-text">
+                                <h4 style="text-align: left">Cities We Serve</h4>
+                                <div id="header">
+                                  <div id="subFooter">
+                                    <div class="subFooterBkg">
+                                      <div class="subShoe">
+                                        <p>
+                                          <span style="font-size: 14px">
+                                            <a href="Cities/city-template.html">CITY TEMPLATE</a> |
+                                            <a href="photo-booth-ann-arbor.html">Ann Arbor</a> |
+                                            <a href="photo-booth-livonia-mi.html">Livonia</a> |
+                                            <a href="photo-booth-birmingham-mi.html">Birmingham</a> |
+                                            <a href="photo-booth-bloomfield-mi.html">Bloomfield</a> |
+                                            <a href="photo-booth-novi-mi.html">Novi</a> |
+                                            <a href="photo-booth-troy-mi.html">Troy</a> |
+                                            <a href="photo-booth-royal-oak-mi.html">Royal Oak</a> |
+                                            <a href="photo-booth-detroit-mi.html">Detroit</a> |
+                                            <a href="photo-booth-southfield-mi.html">Southfield</a> |
+                                            <a href="photo-booth-lansing-mi.html">Lansing</a> |
+                                            <a href="photo-booth-flint-mi.html">Flint</a> |
+                                            <a href="photo-booth-grand-rapids-mi.html">Grand Rapids</a> |
+                                            <a href="photo-booth-jackson-mi.html">Jackson</a> |
+                                            <a href="photo-booth-kalamazoo-mi.html">Kalamazoo</a> |
+                                          </span>
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!--Footer (0 X 0)-->
-        <div class="fl-col fl-node-59078ee664f2e fl-col-small" data-node="59078ee664f2e">
-          <div class="fl-col-content fl-node-content">
-            <div class="fl-module fl-module-rich-text fl-node-59078ee664ffa" data-node="59078ee664ffa">
-              <div class="fl-module-content fl-node-content">
-                <div class="fl-rich-text">
-                  <!--Site Links-->
-                  <h4>Links</h4>
-                  <ul>
-                    <li><a href="/RatesAvailability.html">Contact</a></li>
-                    <li> <a href="/FAQs.html">FAQ</a> </li>
-                    <li> <a href="/cities.html">Cities We Serve</a> </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!--Footer (0 0 X)-->
-        <div class="fl-col fl-node-59078ee664ef4 fl-col-small" data-node="59078ee664ef4">
-          <div class="fl-col-content fl-node-content">
-            <!--Reach Out Contact Info-->
-            <div class="fl-module fl-module-rich-text fl-node-59079189674fc" data-node="59079189674fc">
-              <div class="fl-module-content fl-node-content">
-                <div class="fl-rich-text">
-
-                <h4>Reach Out</h4>
-                <p>
-                  Snap Booth Photos<br />
-                  Milan, MI 48160<br />
-                  <a href="tel:">(734) 707-7794</a>
-                </p>
-
-                </div>
-              </div>
-            </div>
-            <!--Social Media Icons-->
-            <div class="fl-module fl-module-pp-social-icons fl-node-5907955f2610f"data-node="5907955f2610f">
-              <div class="fl-module-content fl-node-content">
-                <div class="pp-social-icons pp-social-icons-left pp-social-icons-horizontal pp-responsive-center">
-
-                  <!--FaceBook Icon/Link-->
-                  <span class="pp-social-icon"itemscope itemtype="http://schema.org/Organization">
-                    <link itemprop="url" href=""/>
-                    <a
-                      itemprop="sameAs"
-                      href="https://www.facebook.com/"
-                      target="_blank"
-                      title="Facebook"
-                      aria-label="Facebook"
-                      role="button"
-                      rel="noopener"
-                    >
-                      <i class="fa fa-facebook"></i>
-                    </a>
-                  </span>
-                  <!--Twitter Icon/Link-->
-                  <span class="pp-social-icon"itemscope itemtype="http://schema.org/Organization">
-                    <link itemprop="url" href=""/>
-                    <a
-                      itemprop="sameAs"
-                      href="https://twitter.com/"
-                      target="_blank"
-                      title="Twitter"
-                      aria-label="Twitter"
-                      role="button"
-                      rel="noopener"
-                    >
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </span>
-
-                  <!--Youtube Icon/Link-->
-                  <span class="pp-social-icon"itemscope itemtype="http://schema.org/Organization">
-                    <link itemprop="url" href=""/>
-                    <a
-                      itemprop="sameAs"
-                      href="https://www.youtube.com/"
-                      target="_blank"
-                      title="YouTube"
-                      aria-label="YouTube"
-                      role="button"
-                      rel="noopener"
-                    >
-                      <i class="fa fa-youtube"></i>
-                    </a>
-                  </span>
-
-                  <!--Google Icon/Link change to tiktok?-->
-                  <span class="pp-social-icon" itemscope itemtype="http://schema.org/Organization">
-                    <link itemprop="url" href=""/>
-                    <a
-                      itemprop="sameAs"
-                      href="https://plus.google.com/r"
-                      target="_blank"
-                      title="Google Plus"
-                      aria-label="Google Plus"
-                      role="button"
-                      rel="noopener"
-                    >
-                      <i class="fa fa-google-plus"></i>
-                    </a>
-                  </span>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-------------End(X X X)Footer----------------->
-
-
-
-      <div class="fl-col fl-node-59078ee664ebb fl-col-small" data-node="59078ee664ebb">
-      <div class="fl-col-content fl-node-content">
-        <div class="fl-module fl-module-rich-text fl-node-59079271d91c2" data-node="59079271d91c2">
-          <div class="fl-module-content fl-node-content">
-            <div class="fl-rich-text">
-
-              <!--Event Type Site Links-->
-              <h4>Cities we serve</h4>
-              <ul>
-                <li><a href="Cities/city-template.html">City Template</a></li>
-
-
-
-
-
-
-              </ul>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
 
 
     </div>
